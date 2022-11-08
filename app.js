d4faf3ea155b11850ca0617cfc05920e17167080
Naomi Earle <naomi.earle@highspeedtraining.co.uk>
2022-11-08 12:02:56 +0000

    partials and css

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const blogs = [
+    { title: 'Shih Tzu learns to skateboard', snippet: 'A smart dog in Leeds learned how to skateboard!' },
+    { title: 'Goldendoodle saves a life', snippet: 'A cute Goldendoodle in Bournemouth saved a life today.' },
+    { title: 'French Bulldog steals bed from owner', snippet: 'A spoilt Frenchie in Kent has stolen her owner\'s bed...' }
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create')
+  res.render('create', { title: 'Create' })
 });
 
 app.use((req, res) => {
-  res.status(404).render('404');
+  res.status(404).render('404', { title: '404' });
 })
